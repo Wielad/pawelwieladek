2266a0708cc72fef80fcd11fdac327c730b3f071
Pawel Wieladek <pawel.wieladek@allegrogroup.com>
2015-12-11 15:29:05 +0100

    Deploy run script

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var githubPages = require('gulp-gh-pages');
 
-gulp.task('publish', function() {
+gulp.task('deploy', function() {
   return gulp.src('./public/**/*')
     .pipe(githubPages({
       remoteUrl: 'https://github.com/pawelwieladek/pawelwieladek.github.io.git',
