8d106527865408421bbdc3b45975cc188fb1b14a
Pawel Wieladek <pawel.wieladek@allegrogroup.com>
2015-12-11 16:30:11 +0100

    Job images

--- src/scripts/components/job.js
+++ src/scripts/components/job.js
@@ -7,13 +7,19 @@ export default class Job extends Component {
         from: PropTypes.string.isRequired,
         to: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        imageSrc: PropTypes.string,
     }
     render() {
         return (
             <div className="job">
-                <h3>{this.props.employer}</h3>
-                <h4 className="date-range">{this.props.from} - {this.props.to}</h4>
-                <p>{this.props.description}</p>
+                <div className="image-container">
+                    <img src={this.props.imageSrc} />
+                </div>
+                <div className="description-container">
+                    <h3>{this.props.employer}</h3>
+                    <h4 className="date-range">{this.props.from} - {this.props.to}</h4>
+                    <p>{this.props.description}</p>
+                </div>
             </div>
         );
     }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../styles/styles.scss';
-import data from 'resume-data';
+import data from '../data/resume-data';
 import Resume from './pages/resume';
 
 ReactDOM.render(
